15fb3176a0223fb9da836d7bfa806153db84a9a0
{mindot7} <{codemj11@gmail.com}>
2022-04-23 21:28:26 +0900

    0423구현

--- 4. 구현/실전/7.럭키_스트레이트.py
+++ 4. 구현/실전/7.럭키_스트레이트.py
@@ -3,10 +3,10 @@ left = 0
 right = 0
 mid = len(n)//2
 
-for i in range(mid):
+for i in range(mid):  # 1번부터  (mid-1)번 인덱스까지
     left += n[i]
 
-for j in range(mid, len(n)):
+for j in range(mid, len(n)):  # (mid)번 인덱스부터 마지막 인덱스까지
     right += n[j]
 
 if left == right:
