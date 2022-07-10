a35ca045bff06a15f11b802d34f1a39c4482d446
mindot7 <codemj11@gmail.com>
2022-07-10 23:45:18 +0900

    0710React

--- React/react-0603/react-for-mj/src/App.js
+++ React/react-0603/react-for-mj/src/App.js
@@ -1,57 +1,74 @@
-import { useState, useEffect } from "react"
-
-function Hello() {
-  function byeFn() {
-    console.log("bye :(")
-  }
-  function hiFn() {
-    console.log("created :)")
-    return byeFn
-  }
-  useEffect(hiFn, [])
-  return <h1>Hello</h1>
-}
+// import { useState } from "react"
 
 // function App() {
-//   const [counter, setValue] = useState(0)
-//   const [keyword, setKeyword] = useState("")
-//   const onClick = () => setValue((prev) => prev + 1)
-//   const onChange = (event) => setKeyword(event.target.value)
-//   useEffect(() => {
-//     console.log("I run only once.")
-//   }, [])
-//   useEffect(() => {
-//     console.log("I run when 'keyword' changes.")
-//   }, [keyword])
-//   useEffect(() => {
-//     console.log("I run when 'counter' changes.")
-//   }, [counter])
-//   useEffect(() => {
-//     console.log("I run when keyword & counter change.")
-//   }, [keyword, counter])
-//   return (
-//     <div>
-//       <input 
-//        value={keyword}
-//        onChange={onChange} 
-//        type="text"
-//        placeholder="Search here..." 
-//       />
-//       <h1>{counter}</h1>
-//       <button onClick={onClick}>Click Me</button>
-//     </div>
-//   )
-// }
+//   const [toDo, setToDo] = useState("")
+//   const [toDos, setToDos] = useState([])
+//   const onChange = (event) => setToDo(event.target.value)
+//   const onSubmit = (event) => {
+//     event.preventDefault()
+//     if (toDo === "") {
+//       return
+//     }
+//     setToDos((currentArray) => [toDo, ...currentArray])
+//     setToDo("")
+//   }
+//   console.log(toDos)
+//   console.log(toDos.map((item, index) => <li key={index}>{item}</li>))
+//     return (
+//       <div>
+//         <h1>My To Dos ({toDos.length})</h1>
+//         <form onSubmit={onSubmit}>
+//           <input
+//             onChange = {onChange}
+//             value={toDo}
+//             type="text"
+//             placeholder="Write your to do..."
+//           />
+//           <button>Add To Do</button>
+//         </form>
+//         <hr />
+//         {toDos.map()}
+//       </div>
+//     )
+//   }
+
+// export default App
+
+import { useState } from "react"
 
 function App() {
-  const [showing, setShowing] = useState(false)
-  const onClick = () => setShowing((prev) => !prev)
+  const [toDo, setToDo] = useState("")
+  const [toDos, setToDos] = useState([])
+  const onChange = (event) => setToDo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if (toDo === "") {
+      return
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
+  console.log(toDos.map((item, index) => <li key={index}>{item}</li>))
   return (
-   <div>
-    {showing ? <Hello /> : null}
-    <button onClick={onClick}>{showing ? "Hide": "Show"}</button>
-  </div>
-  )
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
 export default App;
---
+++ React/react-0603/react7.md
@@ -0,0 +1,58 @@
+## 7.0 To Do List part.1
+
+```js
+// App.js
+
+import { useState } from "react"
+
+function App() {
+  const [toDo, setToDo] = useState("")
+  const [toDos, setToDos] = useState([])
+  const onChange = (event) => setToDo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if(toDo === ""){
+      return
+    }
+    setToDos((currentArray) => [toDo, ...currentArray])
+    setToDo("")
+  }
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange = {onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
+```
+
+
+
+
+
+
+
+## 7.1 To Do List part.2
+
+- 함수를 보내는 방법
+
+  - `setToDos((currentArray) => [toDo, ...currentArray])`
+
+    - 함수의 첫번째 argument를 현재 state로 보낸다 --> 현재 state를 계산하거나 새로운 state를 만들 수 있다
+    - 현재 toDos를 받아와서 새로운 toDo의 array로 return
+
+    
+
+    
+
+    
