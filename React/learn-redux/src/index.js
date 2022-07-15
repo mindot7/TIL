5884c1e983eab8d221b74ab0b2e3907073f659b5
mindot7 <codemj11@gmail.com>
2022-07-15 20:38:47 +0900

    Docs: 0715 Redux study

--- React/learn-redux/src/index.js
+++ React/learn-redux/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './modules'
+
+const store = createStore(rootReducer)  // 스토어 만들기
+console.log(store.getState())  // 스토어의 상태 확인
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
